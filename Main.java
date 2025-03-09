742453e1156ce90b5198276096dcf3192a44c76a
LilyNkantah <nkantahlily1@gmail.com>
2025-03-09 16:28:44 +0000

    adding panels into frame

--- Main.java
+++ Main.java
@@ -5,34 +5,45 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         ImageIcon image = new ImageIcon("images/solo-leveling.jpeg");
-        Border border = BorderFactory.createLineBorder(new Color(89, 97, 148), 5); //create a border
 
-        // JLabel is a GUI component that functions as a display area for strings of text and images. Note that layout
-        // manager by default sets label to take up entire frame
-        JLabel label = new JLabel(); //create a label
-        label.setText("Arise"); //set text of label
+        JLabel label = new JLabel();
+        label.setText("We need a hero!");
         label.setIcon(image);
-        label.setHorizontalTextPosition(JLabel.CENTER); //set text LEFT, CENTER, or RIGHT of image
-        label.setVerticalTextPosition(JLabel.TOP); //set text TOP, CENTER, BOTTOM of image
-        label.setForeground(new Color(89, 97, 148)); //set font colour
-        label.setFont(new Font("MV Boli", Font.ITALIC, 15)); //set text font
-        label.setIconTextGap(10); //set gap of text to image
-        label.setBackground(new Color(255, 250, 115)); //set background colour
-        label.setOpaque(true); //display background colour
-        label.setBorder(border); //set border for label
-        label.setVerticalAlignment(JLabel.CENTER); //set vertical position of label (contents of label i.e. text and image)
-        label.setHorizontalAlignment(JLabel.CENTER); //set horizontal position of label
-        label.setBounds(50, 50, 350, 350); //set position of top left, top right corner of label, width, height of label
+        //label.setVerticalAlignment(JLabel.BOTTOM); //only needed when using a border layout
+        //label.setHorizontalAlignment(JLabel.RIGHT);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setIconTextGap(-1);
+        label.setBounds(100, 25, 220, 220);
 
-        JFrame frame = new JFrame(); //don't have to give it a name if you don't plan on using its name, -> if you
-        // just need to create a frame, do new MyFrame();
+        // JPanel is a GUI component that acts as a container to hold other components
+        JPanel pinkPanel = new JPanel();
+        pinkPanel.setBackground(new Color(255, 148, 148)); //set colour of the panel
+        pinkPanel.setBounds(0, 0, 250, 250); //set position of the panel
+        pinkPanel.setLayout(null);
+
+        JPanel tealPanel = new JPanel();
+        tealPanel.setBackground(new Color(157, 250, 208)); //set colour of the panel
+        tealPanel.setBounds(250, 0, 250, 250); //set position of the panel
+        tealPanel.setLayout(null);
+
+        JPanel limePanel = new JPanel();
+        limePanel.setBackground(new Color(223, 250, 160)); //set colour of the panel
+        limePanel.setBounds(0, 250, 500, 250); //set position of the panel
+        limePanel.setLayout(null); //using a border layout for the panel
+
+        JFrame frame = new JFrame();
+        frame.setTitle("My Java Swing App");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setLayout(null); //setting layout manager to null so we can manually control size of label
+        frame.setSize(750, 750);
         frame.setVisible(true);
-        frame.add(label); // add the label to the frame
-        //frame.pack(); resizes size of frame to accommodate all components in the frame. layout manager needs to be on,
-        // and label bounds cannot be manually set, frame size cannot be manually set
+        frame.setLayout(null);
 
+        limePanel.add(label); //adding a label into a panel. label can only be in one panel at a time
+        //pinkPanel.add(label);
+        //tealPanel.add(label);
+        frame.add(pinkPanel);
+        frame.add(tealPanel);
+        frame.add(limePanel);
     }
 }
